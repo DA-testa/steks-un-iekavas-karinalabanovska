2a41d83cdaf567f00108d2bca6857aacd9de4085
Karīna Labanovska-Sekača <125558305+karinalabanovska@users.noreply.github.com>
2023-02-18 18:11:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ def are_matching(left, right):
 def find_mismatch(text):
     bracket_dict = {'(': ')', '[': ']', '{': '}'}
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
+    for i, c in enumerate(text):
+        if c in "([{":
             # Process opening bracket
-            opening_brackets_stack.append(Bracket(next, i))
+            opening_brackets_stack.append(Bracket(c, i))
 
-        if next in ")]}":
+        if c in ")]}":
             # Process closing bracket
             if len(opening_brackets_stack) == 0:
                 return i + 1
 
             top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next):
+            if not are_matching(top.char, c):
                 return i + 1
 
     if len(opening_brackets_stack) == 0:
@@ -36,7 +36,6 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     print(mismatch)
-
 
 
 if __name__ == "__main__":
